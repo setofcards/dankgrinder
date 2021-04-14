ce3450829abaa5cae678a0cb0f567561f18d1499
setofcards <82534590+setofcards@users.noreply.github.com>
2021-04-14 13:34:57 -0400

    shitly coded income/balance checks to webhook
    dont judge man i barely learned this language 2 hours ago

--- instance/balancecheck.go
+++ instance/balancecheck.go
@@ -16,7 +16,53 @@ import (
 	"github.com/dankgrinder/dankgrinder/instance/scheduler"
 
 	"github.com/dankgrinder/dankgrinder/discord"
+
+	"github.com/nickname32/discordhook"
 )
+
+func (in *Instance) makebalancerequest(balance string) {
+	if in.Features.BalanceCheck.Enable {
+		var id, token = in.Features.BalanceCheck.WebhookID, in.Features.BalanceCheck.WebhookToken
+		wa, err := discordhook.NewWebhookAPI(id, token, true, nil)
+		if err!= nil {
+			panic(err)
+		}
+		msg, err := wa.Execute(nil, &discordhook.WebhookExecuteParams {
+			Username: in.Client.User.Username,
+			Embeds: []*discordhook.Embed{
+				{
+					Title: "Balance Check",
+					Color: 0x00ff00,
+					Description: "current balance: " + balance,
+				},
+			},
+		}, nil, "")
+
+		fmt.Println(msg.ID)
+	}
+}
+
+func (in *Instance) makeincomerequest(income string) {
+	if in.Features.BalanceCheck.Enable {
+		var id, token = in.Features.BalanceCheck.WebhookID, in.Features.BalanceCheck.WebhookToken
+		wa, err := discordhook.NewWebhookAPI(id, token, true, nil)
+		if err!= nil {
+			panic(err)
+		}
+
+		msg, err := wa.Execute(nil, &discordhook.WebhookExecuteParams {
+			Username: in.Client.User.Username,
+			Embeds: []*discordhook.Embed{
+				{
+					Title: "Average Income Check",
+					Color: 0x00ff00,
+					Description: "average income per hour: " + income,
+				},
+			},
+		}, nil, "")
+		fmt.Println(msg.ID)
+	}
+}
 
 func (in *Instance) balanceCheck(msg discord.Message) {
 	if !strings.Contains(msg.Embeds[0].Title, in.Client.User.Username) {
@@ -55,6 +101,9 @@ func (in *Instance) updateBalance(balance int) {
 		numFmt.Sprintf("%d", balance),
 	)
 
+	in.makebalancerequest(
+		numFmt.Sprintf("%d", balance),
+	)
 	if in.startingTime.IsZero() {
 		in.initialBalance = balance
 		in.startingTime = time.Now()
@@ -64,8 +113,13 @@ func (in *Instance) updateBalance(balance int) {
 	inc := balance - in.initialBalance
 	per := time.Now().Sub(in.startingTime)
 	hourlyInc := int(math.Round(float64(inc) / per.Hours()))
+	
 	in.Logger.Infof(
 		"average income: %v coins/h",
 		numFmt.Sprintf("%d", hourlyInc),
 	)
+	in.makeincomerequest(
+		numFmt.Sprintf("%d", hourlyInc),
+	)
 }
+
